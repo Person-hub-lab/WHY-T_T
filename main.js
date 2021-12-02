158656dd2935ef6d2c67ba2df78f5faa79c9d9dd
Person-hub-lab <79429471+Person-hub-lab@users.noreply.github.com>
2021-12-01 21:43:02 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+Webcam.set({
+ width:350,
+ height:300,
+ image_format : 'png',
+ png_quality:100
+});
+
+function view(){
+    save('image.png');
+    console.log("Saved!");
+}
+
+function takeimage(){
+  
+}
